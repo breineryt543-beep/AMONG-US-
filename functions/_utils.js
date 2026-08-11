a72623c20cc2c01cae59e0bb850bb2d91a205027
breineryt543-beep <breineryt543@gmail.com>
2026-08-10 19:44:16 -0500

    Create _utils.js

---
+++ functions/_utils.js
@@ -0,0 +1,15 @@
+export async function hashPassword(password, salt) {
+  const enc = new TextEncoder();
+  const data = enc.encode(`${salt}::${password}::mision-lunar`);
+  const digest = await crypto.subtle.digest("SHA-256", data);
+  return Array.from(new Uint8Array(digest))
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+}
+
+export function json(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: { "Content-Type": "application/json" },
+  });
+}
